3ce42111bd63db8407fb81818fd9b7cac8ec557f
赵思 <zhaosi@yoyohr.com>
2022-10-12 16:21:05 +0800

    删除了对el-form的全局样式，增加了el的自动引入配置，可以不用手动引入message的样式了

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,8 +1,4 @@
 import 'element-plus/theme-chalk/display.css'
-import 'element-plus/es/components/message-box/style/index'
-import 'element-plus/es/components/message/style/index'
-// 为了解决 unplugin-vue-components 自动插入的样式优先级覆盖其他组件自带样式
-import 'element-plus/es/components/icon/style/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // // 做全局引入，挂载到this上 这两个组件内部已经实现了globalProperties
